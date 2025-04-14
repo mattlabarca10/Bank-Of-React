9824f98ae34dba82748042d16f50c933096592b9
Matthew LaBarca <matthewlabarca@Matthews-MacBook-Pro-2.local>
2025-04-13 21:03:14 -0400

    completed logic to dispaly and add credits and debits successfully

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,27 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+  // Create the list of Credit items
+  const creditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each credits JSON array element
+      let date = credit.date.slice(0,10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    });
+  }
   return (
     <div>
       <h1>Credits</h1>
+      <AccountBalance accountBalance={props.accountBalance} />
+      {creditsView()}
+      <form onSubmit={props.addCredit}>
+        <input type="text" name="description" placeholder="description"/>
+        <input type="number" name="amount" placeholder="amount"/>
+        <button type="submit">Add Credit</button>
+      </form>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
